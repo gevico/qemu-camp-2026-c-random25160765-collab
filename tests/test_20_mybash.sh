660e76324e39ddc7de065f171808e99d47df3776
rd <random25160765@gmail.com>
2026-04-06 20:17:37 +0800

    fix: 尝试修复由于Makefile变量名不规范导致的Github Action错误x6

---
+++ tests/test_20_mybash.sh
@@ -0,0 +1,8 @@
+myfile /home/rd/courses/qemu-camp-2026-c-random25160765-collab/exercises/20_mybash/bin/mybash
+myfile /home/rd/courses/qemu-camp-2026-c-random25160765-collab/exercises/20_mybash/obj/mybash/main.o
+
+mysed s/unix/linux/ "unix is opensource. unix is free os."
+
+mytrans /home/rd/courses/qemu-camp-2026-c-random25160765-collab/exercises/20_mybash/src/mytrans/text.txt
+
+mywc /home/rd/courses/qemu-camp-2026-c-random25160765-collab/exercises/20_mybash/src/mytrans/text.txt
